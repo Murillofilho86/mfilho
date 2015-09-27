5b92463362dac6780b4cf9c03a403eef17371cf9
Murillo Fillho <murillohard@hotmail.com>
2015-09-26 21:41:13 -0300

    CadeMeuMedico 1.0.2

---
+++ CadeMeuMedico/Models/MedicoMetadado.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CadeMeuMedico.Models
+{
+    [MetadataType(typeof(MedicoMetadado))]
+    public partial class Medico
+    {
+
+    }
+
+    public class MedicoMetadado
+    {
+        [Required(ErrorMessage= "Obrigatório informar o CRM")]
+        [StringLength(30, ErrorMessage="O CRM deve possuir no máximo 30 caracteres")]
+        public string CRM { get; set; }
+
+        [Required(ErrorMessage="Obrigatório informar o Nome")]
+        [StringLength(80, ErrorMessage="O Nome deve possuir no máximo 80 caracteres")]
+        public string nome { get; set; }
+
+        [Required(ErrorMessage="Obrigatório informar o endereço")]
+        [StringLength(100, ErrorMessage="O endereço deve possuir no máximo 100 caracteres")]
+        public string endereco { get; set; }
+
+        [Required(ErrorMessage="Obrigatorio informar o bairro")]
+        [StringLength(60, ErrorMessage="O bairro deve possuir no máximo 60 caracteres")]
+        public string bairro { get; set; }
+
+        [Required(ErrorMessage="Obrigatório informar o email")]
+        [StringLength(100, ErrorMessage="O email deve possuir no máximo 100 caracteres")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage="Obrigatório informar se atende por convenio")]
+        public bool atendePorConvenio { get; set; }
+
+        [Required(ErrorMessage="Obrigatório informar se tem clinica")]
+        public bool TemClinica { get; set; }
+
+        [StringLength(80, ErrorMessage="O website deve possuir no máximo 80 caracteres")]
+        public string webSiteBlog { get; set; }
+
+        [Required(ErrorMessage="Obrigatório informar a cidade")]
+        public int cidadeID { get; set; }
+
+        [Required(ErrorMessage="Obrigatório informar a especialidade")]
+        public int especialidadeID { get; set; }
+    }
+}
---
+++ CadeMeuMedico/Models/UsuariosMetadado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CadeMeuMedico.Models
+{
+    [MetadataType(typeof(MedicoMetadado))]
+    public partial class usuarios
+    {
+
+    }
+
+
+
+    public class UsuariosMetadado
+    {
+
+        [Required(ErrorMessage="Obrigatório informar o nome")]
+        [StringLength(80, ErrorMessage="O nome deve possuir no máximo 80 caracteres")]
+        public string nome { get; set;}
+
+        [Required(ErrorMessage="Obrigatório informar o login")]
+        [StringLength(30, ErrorMessage="O login deve possuir no máximo 30 caracteres")]
+        public string login{get; set; }
+
+        [Required(ErrorMessage="Obrigatório informar a senha")]
+        [StringLength(100, ErrorMessage="A senha deve possuir no máximo 100 caracteres")]
+        public string senha { get; set; }
+
+        [Required(ErrorMessage="Obrigatório informa o email")]
+        [StringLength(100, ErrorMessage="O email deve possuir no máximo 100 caracteres")]
+        public string email { get; set; }
+
+
+    }
+}
